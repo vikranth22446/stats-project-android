9599137fe6fdca6cfa209b444a6717bf23ce4bfb
Vikranth Srivatsa <rama22446@gmail.com>
2017-05-17 08:47:46 -0700

    Update Splash and icosn

---
+++ app/src/main/java/com/vikranth/stats_account/SplashActivity.java
@@ -0,0 +1,20 @@
+package com.vikranth.stats_account;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by vikranth on 5/15/17.
+ */
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
